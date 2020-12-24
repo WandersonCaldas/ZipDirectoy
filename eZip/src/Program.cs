68cea1f72383bc86e82dc024eb0b7ba36b5deb06
Wanderson Caldas <wcaldasti@gmail.com>
2020-12-23 22:26:49 -0300

    CRIAR APLICAÇÃO

---
+++ eZip/src/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Threading;
+using System.Globalization;
+using System.Configuration;
+
+namespace src
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var culture = new CultureInfo("pt-BR");
+            Thread.CurrentThread.CurrentCulture = culture;
+            Thread.CurrentThread.CurrentUICulture = culture;
+
+            Console.WriteLine("ROTINA INICIADA");
+
+            string _caminho_origem = ConfigurationManager.AppSettings.Get("caminho_origem");
+            string _caminho_destino = ConfigurationManager.AppSettings.Get("caminho_destino");
+            
+            System.IO.Compression.ZipFile.CreateFromDirectory(_caminho_origem, _caminho_destino);
+
+            Console.WriteLine("ROTINA FINALIZADA");
+            Console.ReadKey();
+        }
+    }
+}
